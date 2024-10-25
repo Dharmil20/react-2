6f2d6627ae2754243c7125249fa7c2147f03cd5e
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-25 16:36:09 +0000

    Add usePrev

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { useFetch } from './hooks/useFetch';
+import { usePrev } from './hooks/usePrev';
 
 function App() {
   const [currTodo, setCurrTodo] = useState(1);
   const { count, handleCount } = useCounter();
-  const {data, isLoading} = useFetch("https://jsonplaceholder.typicode.com/todos/" + currTodo, 10);
+  const [state, setState] = useState(0);
+  const prev = usePrev(state);
+  const { data, isLoading } = useFetch("https://jsonplaceholder.typicode.com/todos/" + currTodo, 10);
 
   return (
     <>
@@ -13,7 +16,10 @@ function App() {
       <button onClick={() => setCurrTodo(1)}>1</button>
       <button onClick={() => setCurrTodo(2)}>2</button>
       <button onClick={() => setCurrTodo(3)}>3</button> <br />
-      {isLoading ? "Loading......" : data.title}
+      {isLoading ? "Loading......" : data.title} <br /><br />
+      {state} <br />
+      <button onClick={() => setState(c => c + 1)}>inc</button> <br />
+      Privious Value was {prev}
     </>
   )
 }
