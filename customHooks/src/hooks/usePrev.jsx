0971a42a2b193482632d6ed3faeaa810cd9290e8
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-26 13:37:00 +0000

    Add usePrev v2

--- customHooks/src/hooks/usePrev.jsx
+++ customHooks/src/hooks/usePrev.jsx
@@ -2,8 +2,18 @@ import { useEffect, useRef } from "react";
 
 export function usePrev(value){
     const ref = useRef();
+
     useEffect(()=>{
         ref.current = value
     }, [value])
-    return ref.current;
+
+    return ref.current; 
+    //React property: It returns first, useEffect gets called later 
 }
+//Refs dont re-render when they change
+/*
+Eska Implementation aisa hai ki: 
+1. initially value 0 hai; we return ref.current ie undifined;  lastly useEffect is called due to value as a dependency which changes ref.current to 0
+2. value updates to 1; we return ref.current ie 0; lastly useEffect is called due to value as a dependency which changes ref.current to 1
+3. value updates to 2; we return ref.current ie 1 (from previous); ref.current updated to 2
+*/
